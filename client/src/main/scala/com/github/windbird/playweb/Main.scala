905a0af97b3b1fa6d770e134c67e9e51077ba98e
windbird123 <windbird123@gmail.com>
2023-12-05 16:34:15 +0900

    fix routing

--- client/src/main/scala/com/github/windbird/playweb/Main.scala
+++ client/src/main/scala/com/github/windbird/playweb/Main.scala
@@ -10,11 +10,58 @@ import frontroute._
 import org.scalajs.dom
 import org.scalajs.dom.HTMLDivElement
 
-object SideMenu {
-  val Home: String      = "Home"
-  val Component: String = "Component"
-  val Facade: String    = "Facade"
-  val Network: String   = "Network"
+case class Menu(name: String, path: String, iconCls: String)
+object Menu {
+  val Home: Menu      = Menu("Home", "/", "fas fa-home me-2")
+  val Component: Menu = Menu("Component", "component", "fas fa-user me-2")
+  val Facade: Menu    = Menu("Facade", "facade", "fas fa-marker me-2")
+  val Network: Menu   = Menu("Network", "network", "fas fa-envelope-open-text me-2")
+}
+
+class Layout {
+  private val selectedMenu: Var[Menu] = Var(Menu.Home)
+
+  private def navi(menu: Menu): ReactiveHtmlElement[HTMLDivElement] =
+    div(
+      cls := "flex-column nav nav-pills",
+      a(
+        cls := "nav-link",
+        href := menu.path,
+        i(cls := menu.iconCls),
+        span(menu.name)
+      ).amend(
+        cls.toggle("active") <-- selectedMenu.signal.map(_ == menu),
+        onClick.mapTo(menu) --> selectedMenu
+      )
+    )
+
+  private def sidebar(): ReactiveHtmlElement[HTMLDivElement] =
+    div(
+      cls := "sidebar",
+      div(
+        cls := "sidebar-header",
+        img(
+          src := "https://images.plot.ly/logo/new-branding/plotly-logomark.png",
+          width := "3rem"
+        ),
+        h2("Sidebar")
+      ),
+      hr(),
+      navi(Menu.Home),
+      navi(Menu.Component),
+      navi(Menu.Facade),
+      navi(Menu.Network)
+    )
+
+  def build(menu: Menu, content: HtmlElement): HtmlElement =
+    div(
+      EventStream.fromValue(menu) --> selectedMenu,
+      sidebar(),
+      div(
+        cls := "content",
+        content
+      )
+    )
 }
 
 object Main {
@@ -27,65 +74,26 @@ object Main {
     )
   }
 
-  def sidebar(selectedMenu: Var[String]): ReactiveHtmlElement[HTMLDivElement] = {
-    def navi(
-      name: String,
-      url: String,
-      iconCls: String,
-      selectedMenu: Var[String]
-    ): ReactiveHtmlElement[HTMLDivElement] =
-      div(
-        cls := "flex-column nav nav-pills",
-        a(
-          cls := "nav-link",
-          href := url,
-          i(cls := iconCls),
-          span(name)
-        ).amend(
-          cls.toggle("active") <-- selectedMenu.signal.map(_ == name),
-          onClick.mapTo(name) --> selectedMenu
-        )
-      )
-
-    div(
-      cls := "sidebar",
-      div(
-        cls := "sidebar-header",
-        img(
-          src := "https://images.plot.ly/logo/new-branding/plotly-logomark.png",
-          width := "3rem"
-        ),
-        h2("Sidebar")
-      ),
-      hr(),
-      navi(SideMenu.Home, url = "/", iconCls = "fas fa-home me-2", selectedMenu),
-      navi(SideMenu.Component, url = "component", iconCls = "fas fa-user me-2", selectedMenu),
-      navi(SideMenu.Facade, url = "facade", iconCls = "fas fa-marker me-2", selectedMenu),
-      navi(SideMenu.Network, url = "network", iconCls = "fas fa-envelope-open-text me-2", selectedMenu)
-    )
-  }
-
-  def layout(content: HtmlElement, selectedMenu: Var[String]): HtmlElement =
-    div(
-      sidebar(selectedMenu),
-      div(
-        cls := "content",
-        content
-      )
-    )
-
   def main(args: Array[String]): Unit = {
-    val selectedMenu: Var[String] = Var(SideMenu.Home)
+    val layout: Layout = new Layout()
 
+    // routing 의 HtmlElement 는
+    // passed by-name, HtmlElement will not be evaluated until it's needed
     val app = div(
-      initRouting,                                                 // routing 을 위해 반드시 포함되어야 함
-      LinkHandler.bind,                                            // anchor 에 대한 browser URL 업데이트를 위함
-      pathEnd(layout(HomeDemo.demo, selectedMenu)),                // home
-      path("component")(layout(ComponentDemo.demo, selectedMenu)), // component
-      (path("network") & maybeParam("url")) { urlOpt => // network
-        layout(network(urlOpt.getOrElse("")), selectedMenu)
+      initRouting,      // routing 을 위해 반드시 포함되어야 함
+      LinkHandler.bind, // anchor 에 대한 browser URL 업데이트를 위함 (https://frontroute.dev/v/0.18.x/getting-started/links-and-navigation)
+      pathEnd { // home
+        layout.build(Menu.Home, HomeDemo.demo)
       },
-      path("facade")(layout(FacadeDemo.demo, selectedMenu))        // facade
+      path("component") {                                // component
+        layout.build(Menu.Component, ComponentDemo.demo)
+      },
+      (path("network") & maybeParam("url")) { urlOpt => // network
+        layout.build(Menu.Network, network(urlOpt.getOrElse("")))
+      },
+      path("facade") { // facade
+        layout.build(Menu.Facade, FacadeDemo.demo)
+      }
     )
 
     val containerNode = dom.document.getElementById("main_content")
