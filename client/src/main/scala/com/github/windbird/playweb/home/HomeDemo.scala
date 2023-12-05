c6fb4cef73dbaf1429c314c578315e5a99607d3e
windbird123 <windbird123@gmail.com>
2023-12-05 14:21:52 +0900

    adds menu

--- client/src/main/scala/com/github/windbird/playweb/home/HomeDemo.scala
+++ client/src/main/scala/com/github/windbird/playweb/home/HomeDemo.scala
@@ -1,13 +1,27 @@
 package com.github.windbird.playweb.home
 import com.raquo.laminar.api.L._
 import com.raquo.laminar.nodes.ReactiveHtmlElement
-import org.scalajs.dom.HTMLDivElement
+import org.scalajs.dom.{HTMLAnchorElement, HTMLDivElement}
 
 object HomeDemo {
+  def make_anchor(description: String, link: String): ReactiveHtmlElement[HTMLAnchorElement] =
+    a(description, href := link, target := "_blank", display := "block")
+
   val demo: ReactiveHtmlElement[HTMLDivElement] = div(
     cls := "container mt-3",
-    a("component", href := "/component", target := "_blank", display := "block"),
-    a("route", href := "/blog", target := "_blank", display := "block"),
-    a("facade", href := "/facade", target := "_blank", display := "block")
+    h1("개요"),
+    ul(
+      li(make_anchor("개발 설명 블로그", "https://wefree.tistory.com/324")),
+      li(make_anchor("소스 코드 github", "https://github.com/windbird123/play-web"))
+    ),
+    h1("메뉴"),
+    ul(
+      li(make_anchor("component", "/component")),
+      span("Laminar 에서 bootstrap css 의 사용"),
+      li(make_anchor("network", "/network")),
+      span("URL path, query param 에 호출 정보 포함하기"),
+      li(make_anchor("facade", "/facade")),
+      span("javascripts library 를 scala.js facade 로 만들기")
+    )
   )
 }
--- client/src/main/scala/com/github/windbird/playweb/network/NetworkDemo.scala
+++ client/src/main/scala/com/github/windbird/playweb/network/NetworkDemo.scala
@@ -27,10 +27,8 @@ object NetworkDemo {
     )
 
   // Browser URL 관련 모든 Var 들 값을 확인해 업데이트 한다.
-  def updateBrowserUrl(): Unit = {
-    val requestUrl = urlVar.now()
-    BrowserNavigation.pushState(url = s"/blog?url=${URIUtils.encodeURI(requestUrl)}")
-  }
+  def updateBrowserUrl(requestUrl: String): Unit =
+    BrowserNavigation.pushState(url = s"/network?url=${URIUtils.encodeURI(requestUrl)}")
 
   val inputGroup: ReactiveHtmlElement[HTMLDivElement] = div(
     cls := "input-group mb-3",
@@ -39,8 +37,9 @@ object NetworkDemo {
     ),
     button.amend(
       onClick --> { _ =>
-        urlVar.update(_ => textInput.ref.value)
-        updateBrowserUrl()
+        val requestUrl: String = textInput.ref.value
+        urlVar.update(_ => requestUrl)
+        updateBrowserUrl(requestUrl)
       }
     )
   )
@@ -50,19 +49,19 @@ object NetworkDemo {
       Util.summary("Routing Demo", None),
       p(
         cls := "lead",
-        """https://wefree-scalajs.fly.dev/blog?url=https%3A%2F%2Fapi.zippopotam.us%2Fus%2F90210 처럼 요청 하면, input form 에
+        """https://wefree-scalajs.fly.dev/network?url=https%3A%2F%2Fapi.zippopotam.us%2Fus%2F90210 처럼 요청 하면, input form 에
           |url param 으로 지정된 값이 채워지고, 요청된 결과까지 화면에 보여준다.
           |(자동으로 submit 버튼을 누른 것과 같은 효과)
           |""".stripMargin
       ),
       a(
         "request: https://api.zippopotam.us/us/90210",
-        href := "/blog?url=https%3A%2F%2Fapi.zippopotam.us%2Fus%2F90210",
+        href := "/network?url=https%3A%2F%2Fapi.zippopotam.us%2Fus%2F90210",
         display := "block"
       ),
       a(
         "request: https://jsonplaceholder.typicode.com/todos/2",
-        href := "/blog?url=https%3A%2F%2Fjsonplaceholder.typicode.com%2Ftodos%2F2",
+        href := "/network?url=https%3A%2F%2Fjsonplaceholder.typicode.com%2Ftodos%2F2",
         display := "block"
       ),
       inputGroup,
